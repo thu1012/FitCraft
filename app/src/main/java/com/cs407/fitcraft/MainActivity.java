dab37e07fba109b0665c7601c7d526f49bf2053f
Tairan Hu <tairan1012@gmail.com>
2023-10-20 01:58:55 -0500

    - added buttons to main for testing

--- app/src/main/java/com/cs407/fitcraft/MainActivity.java
+++ app/src/main/java/com/cs407/fitcraft/MainActivity.java
@@ -2,7 +2,9 @@ package com.cs407.fitcraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mainNewWorkoutBtnHandler(View view) {
+        Intent intent = new Intent(this, NewWorkoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void mainAddExerciseBtnHandler(View view) {
+        Intent intent = new Intent(this, AddExerciseActivity.class);
+        startActivity(intent);
     }
 }
