ce04cd7fb00240ee2ca12f5ae94f55d3afac77ad
Nianze Guo <nguo9@wisc.edu>
2023-10-31 18:46:41 -0500

    update action bar

--- app/src/main/java/com/cs407/fitcraft/FirstPage.java
+++ app/src/main/java/com/cs407/fitcraft/FirstPage.java
@@ -10,5 +10,9 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("*Exercise Name*");
+        }
     }
 }
