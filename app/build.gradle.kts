f09343fe0c299f832945e5f5f9a2c5a40bacda88
Tairan Hu <40744575+thu1012@users.noreply.github.com>
2023-10-31 19:09:46 -0500

    Your workout (#2)
    * create a branch
    
    * update
    
    * update
    
    * Add Header
    
    * Add List
    
    * Adjust List
    
    * Add button
    
    * Change button background color
    
    * Finish first page layout
    
    * update action bar
    
    * delete theme:  no action bar
    
    * change gradle
    
    ---------
    
    Co-authored-by: Nianze Guo <nguo9@wisc.edu>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.cs407.fitcraft"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
