350e2ce3b53322971399c8f6a569bceb3c81e5ad
Tairan Hu <tairan1012@gmail.com>
2023-12-01 18:41:45 -0600

    Tested for video playerview

--- app/src/main/java/com/cs407/fitcraft/ExerciseDetails.java
+++ app/src/main/java/com/cs407/fitcraft/ExerciseDetails.java
@@ -1,8 +1,34 @@
 package com.cs407.fitcraft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.VideoView;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.mediacodec.DefaultMediaCodecAdapterFactory;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.TrackSelectionView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ExerciseDetails extends AppCompatActivity {
 
@@ -16,6 +42,8 @@ public class ExerciseDetails extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("*Exercise Name*");
         }
+
+        setVideo(findViewById(R.id.videoView), "TestVideo");
     }
 
     @Override
@@ -26,5 +54,35 @@ public class ExerciseDetails extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setVideo(VideoView videoView, String exerciseName) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Exercises").document(exerciseName)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            String videoUrl = (String) task.getResult().get("url");
+                            FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+                            StorageReference httpsReference = firebaseStorage.getReferenceFromUrl(videoUrl);
+
+                            httpsReference.getDownloadUrl()
+                                    .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Uri> task) {
+                                            if(task.isSuccessful()) {
+
+                                            } else {
+
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Log.e("ERROR", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
