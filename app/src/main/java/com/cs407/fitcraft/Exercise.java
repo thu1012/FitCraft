cc427fb259d51df63c8158d497d01c4ad2354260
Tairan Hu <40744575+thu1012@users.noreply.github.com>
2023-12-08 16:43:09 -0600

    Exercise detail play video (#7)
    * Created Video Setter helper class
    
    * Added intent to play video accordingly

---
+++ app/src/main/java/com/cs407/fitcraft/Exercise.java
@@ -0,0 +1,11 @@
+package com.cs407.fitcraft;
+
+public class Exercise {
+    String name, description, videoUrl, documentName;
+    public Exercise(String name, String description, String videoUrl, String documentName) {
+        this.name = name;
+        this.description = description;
+        this.videoUrl = videoUrl;
+        this.documentName = documentName;
+    }
+}
