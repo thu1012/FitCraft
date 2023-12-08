f18bbb76ca3781f845bfd579f8b1677075f9d6a2
Tairan Hu <tairan1012@gmail.com>
2023-12-08 16:07:49 -0600

    Created Video Setter helper class

--- app/src/main/java/com/cs407/fitcraft/ExerciseDetails.java
+++ app/src/main/java/com/cs407/fitcraft/ExerciseDetails.java
@@ -1,8 +1,22 @@
 package com.cs407.fitcraft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ExerciseDetails extends AppCompatActivity {
 
@@ -11,11 +25,18 @@ public class ExerciseDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_details);
 
+        String exerciseName = getIntent().getStringExtra("exerciseName");
+        if(exerciseName==null) exerciseName = "TestExercise";
+
         // Enable the Up button
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("*Exercise Name*");
+            getSupportActionBar().setTitle("*"+exerciseName+"*");
         }
+
+        VideoSetter videoSetter = new VideoSetter();
+        VideoView videoView = findViewById(R.id.exerciseDetailsVideoView);
+        videoSetter.setVideo(videoView, exerciseName, this);
     }
 
     @Override
